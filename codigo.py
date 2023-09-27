5d8b7875f8c75464c7e7a7dac60228b226693cf8
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2023-09-27 15:27:27 -0300

    Aula 1, automação de tarefas, criação de 'bot'

--- codigo.py
+++ codigo.py
@@ -36,3 +36,32 @@ import pandas
 
 tabela = pandas.read_csv('produtos.csv')
 
+for linha in tabela.index:
+    # Passo 4: Cadastrar o primeiro produto
+    pyautogui.click(x=790, y=304)
+
+
+    # Preencher os campos
+    pyautogui.write(str(tabela.loc[linha, 'codigo']))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, 'marca']))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, 'tipo']))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, 'categoria']))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, 'preco_unitario']))
+    pyautogui.press('tab')  
+    pyautogui.write(str(tabela.loc[linha, 'custo']))
+    pyautogui.press('tab')
+
+    obs = tabela.loc[linha, 'obs']
+
+    if not pandas.isna(obs):
+        pyautogui.write(str(obs))
+            
+    pyautogui.press('tab')  
+    pyautogui.press('enter')
+
+    pyautogui.scroll(50000)
+
